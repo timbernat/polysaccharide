f3fadbad83d562faccd8372b4404a83c1ba311c4
Timotej Bernat <tibe3324@colorado.edu>
2023-03-24 14:16:46 -0600

    Added charging types, moved strip_units to general

--- extratypes.py
+++ extratypes.py
@@ -1,17 +1,22 @@
 from typing import Any, Callable, Union, Optional
-
 from numpy.typing import ArrayLike
 from numpy import number, ndarray
 
-from pint import Quantity as PintQuantity
-from openmm.unit.quantity import Quantity as OMMQuantity
-
-
-
+# Type aliases
+# -- General
 Numeric = Union[int, float, complex, number]
 ArrayLike = Union[list, tuple, ndarray, ArrayLike] # exclude dicts and sets, as they introduce complicated behaviors
 JSONSerializable = Union[str, bool, int, float, tuple, list, dict] 
 
+# -- OpenFF / graph match
+SubstructSummary = tuple[str, list[int], bool]  # RDKit graph-match-specific type alias
+
+# -- Charge mapping and averaging
+ChargeMap = dict[int, float] 
+AtomIDMap = dict[str, dict[int, tuple[int, str]]]
+
+
+# Typechecking functions
 def isnumeric(var: Any) -> bool:
   '''Check if a variable is numerical'''
   return isinstance(var, Numeric.__args__)
@@ -19,15 +24,3 @@ def isnumeric(var: Any) -> bool:
 def isarraylike(var: Any) -> bool:
   '''Check if a variable is numerical'''
   return isinstance(var, ArrayLike.__args__)
-
-def strip_units(coords : Union[tuple, PintQuantity, OMMQuantity]) -> tuple[float]:
-    '''
-    Sanitize coordinate tuples for cases which require unitless quantities
-    Specifically needed since OpenMM and pint each have their own Quantity and Units classes
-    '''
-    if isinstance(coords, PintQuantity):
-        return coords.magnitude
-    elif isinstance(coords, OMMQuantity):
-        return coords._value
-
-    return coords
