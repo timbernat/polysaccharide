f75bf00e608021ef6d7b1fd71743a71ed3e6c6d9
Timotej Bernat <tibe3324@colorado.edu>
2023-03-29 11:43:29 -0600

    Migrated Solvent to solvation

--- solvents.py
+++ solvents.py
@@ -1,49 +1,6 @@
-from pathlib import Path
-from dataclasses import dataclass, field
-from openmm.unit import gram, mole, centimeter
+from .solvation import Solvent
+from openmm.unit import gram, centimeter, mole
 
-N_AVOGADRO = 6.02214076e23 * mole**-1
-
-
-@dataclass
-class Solvent:
-    '''For encapsulating information about solvents'''
-    name    : str
-    formula : str
-    smarts  : str
-
-    density : float
-    MW : float # molecular weight
-
-    charges : dict[int, float] = field(default=None)
-    structure_file : Path      = field(default=None)
-    forcefield_file : Path     = field(default=None)
-
-    @property
-    def number_density(self) -> float:
-        '''
-        Determine the number of solvent molecules per unit volume from known physical constants
-        For best results, provide arguments as Quantities with associated units
-        '''
-        return (self.density / self.MW) * N_AVOGADRO
-
-    @property
-    def monomer_json_data(self):
-        '''Generate a monomer-spec-conformant JSON dictionary entry'''
-        return {
-            "monomers": {
-                self.name : self.smarts
-            },
-            "caps": {
-                self.name : []
-            },
-            "charges" : {
-                self.name : self.charges
-            }
-        }
-
-
-# Define common solvents and their properties here
 WATER_TIP3P = Solvent(
     name='water',
     formula = 'H2O',
