ceb5d4ddf5e4d3276abbbed8bb14493d0402e02b
Timotej Bernat <tibe3324@colorado.edu>
2023-04-04 18:09:45 -0600

    Added RDKit Atom and Mol types

--- extratypes.py
+++ extratypes.py
@@ -1,20 +1,28 @@
-from typing import Any, Callable, Union, Optional
+from typing import Any, Union
 from numpy.typing import ArrayLike
 from numpy import number, ndarray
 
+from rdkit import Chem
+
+
 # Type aliases
-# -- General
+## General
 Numeric = Union[int, float, complex, number]
 ArrayLike = Union[list, tuple, ndarray, ArrayLike] # exclude dicts and sets, as they introduce complicated behaviors
 JSONSerializable = Union[str, bool, int, float, tuple, list, dict] 
 
-# -- OpenFF / graph match
+## OpenFF / graph match
 SubstructSummary = tuple[str, list[int], bool]  # RDKit graph-match-specific type alias
 
-# -- Charge mapping and averaging - TODO : remove these, as they have been migrated over to .charging
+## RDKit
+RDMol  = Chem.rdchem.Mol
+RDAtom = Chem.rdchem.Atom
+
+## Charge mapping and averaging - TODO : remove these, as they have been migrated over to .charging
 AtomIDMap = dict[str, dict[int, tuple[int, str]]]
 ChargeMap = dict[int, float] 
 ResidueChargeMap = dict[str, ChargeMap]
+
 
 # Typechecking functions
 def isnumeric(var: Any) -> bool:
