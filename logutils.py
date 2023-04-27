fb33daa69452aeed34baddc1c43d1b5fc5c093da
Timotej Bernat <tibe3324@colorado.edu>
2023-04-27 17:03:37 -0600

    Ported over timestamp utils, expanded MSFHandler creation

--- logutils.py
+++ logutils.py
@@ -1,18 +1,36 @@
+# Logging imports
 import logging
 from logging import Logger
 from traceback import format_exception
+LOGGER_REGISTRY = lambda : logging.root.manager.loggerDict # dict of all extant Loggers through all loaded modules - NOTE : written as lambda to allow for updating 
 
+# Generic imports
 from pathlib import Path
 from typing import Iterable, Optional, Union
 from datetime import datetime
 
-LOGGER_REGISTRY = lambda : logging.root.manager.loggerDict # dict of all extant Loggers through all loaded modules - NOTE : written as lambda to allow for updating 
+# Date and time formatting
+DATETIME_FMT = '%m-%d-%Y_at_%H-%M-%S_%p' # formatted string which can be used in file names without error
 LOG_FORMATTER   = logging.Formatter('%(asctime)s.%(msecs)03d [%(levelname)-8s:%(module)16s:line %(lineno)-3d] - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
+def extract_time(timestamp : str) -> str:
+    '''De-format a timestamped string and extract just the timestamp'''
+    return datetime.strptime(timestamp, DATETIME_FMT)
+
+def timestamp_now(fmt_str : str=DATETIME_FMT) -> str:
+    '''Return a string timestamped with the current date and time (at the time of calling)
+    Is formatted such that the resulting string can be safely used in a filename'''
+    return datetime.now().strftime(fmt_str)
+
+# File handling classes
 class MultiStreamFileHandler(logging.FileHandler):
-    '''Subclass to cut down boilerplate of logfile I/O for loggers with multiple origins'''
-    def __init__(self, *args, loggers : Union[Logger, list[Logger]]=None, formatter : logging.Formatter=LOG_FORMATTER, proc_name : str='Process', **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    '''Class to simplify logging file I/O given multiple logger sources providing logging input
+    Automatically reports process completion & runtime if process is successful, or detailed error traceback otherwise
+    
+    Can spawn child processes to have multiple nested levels of logging to many partitioned output files for layered processes'''
+    def __init__(self, filename : Union[str, Path], mode : str='a', encoding : Optional[str]=None, delay : bool=False, errors : Optional[str]=None, # FileHandler base args
+                 loggers : Union[Logger, list[Logger]]=None, formatter : logging.Formatter=LOG_FORMATTER, proc_name : str='Process') -> None:       # args specific to this class
+        super().__init__(filename, mode, encoding, delay, errors)
 
         self.proc_name : str = proc_name
         self.id : int = self.__hash__()  # generate unique ID number for tracking children
@@ -35,7 +53,7 @@ class MultiStreamFileHandler(logging.FileHandler):
 
     def subhandler(self, *args, **kwargs) -> 'MultiStreamFileHandler':
         '''Generate a subordinate "child" process which reports back up to the spawning "parent" process once complete'''
-        child = MultiStreamFileHandler(*args, **kwargs)
+        child = self.__class__(*args, **kwargs) # generalizes inheritance much better
         self.children[child.id] = child # register the child for reference
         child.parent = self # assert self as the child's parent (allows child to tell if any parent handlers exist above it)
 
@@ -87,6 +105,30 @@ class MultiStreamFileHandler(logging.FileHandler):
         for logger in self._loggers: # TOSELF : can't just call individual unregister_logger() method, since pop will change list size while iterating (causes some Loggers to be "skipped")
             logger.removeHandler(self)
         self._loggers.clear()
+
+class MultiStreamFileHandlerFlexible(MultiStreamFileHandler):
+    '''MSFHandler which is a bit more flexible (and lazy) when it comes to log file naming and creation
+    Can either pass a log file path (as normal) or a directory into which to output the logfile
+    If the latter is chosen, will create a logfile based on the specified process name, optionally adding a timestamp if desired'''
+    def __init__(self, filedir : Optional[Path]=None, filename : Optional[Union[str, Path]]=None, mode : str='a', encoding : Optional[str]=None, delay : bool=False, errors : Optional[str]=None, # FileHandler base args
+                 loggers : Union[Logger, list[Logger]]=None, formatter : logging.Formatter=LOG_FORMATTER, proc_name : str='Process', timestamp : bool=True) -> None: # new args specific to this class
+        if filename is None:
+            if filedir is None:
+                raise AttributeError('Must specify either a path to log file OR an output directory in which to generate a log file')
+            assert(filedir.is_dir()) # double check that a proper directory has in fact been passed
+            
+            # implicit "else" if no error is raised 
+            filestem = proc_name.replace(' ', '_') # use process name to generate filename; change spaces to underscores for file saving
+            if timestamp:
+                filestem += f'_{timestamp_now()}' 
+            filename = filedir / f'{filestem}.log'
+
+        super().__init__(filename, mode, encoding, delay, errors, loggers, formatter, proc_name)
+
+# alias names to make them less unwieldy
+MSFHandler        = MultiStreamFileHandler
+MSFHandlerFlex    = MultiStreamFileHandlerFlexible
+ProcessLogHandler = MultiStreamFileHandlerFlexible # more inviting name, intended for use as default
 
 # legacy version, kept for backwards compatibility reasons        
 class MultiLogFileHandler(logging.FileHandler):
