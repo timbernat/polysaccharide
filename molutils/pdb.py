e368b55a5e112f1c6577cd7030148cfcf620f831
Timotej Bernat <tibe3324@colorado.edu>
2023-05-03 17:50:20 -0600

    Expanded strip_water to be more explicit than partial

--- molutils/pdb.py
+++ molutils/pdb.py
@@ -1,7 +1,10 @@
 '''Tools for manipulating PDB structure and trajectory files'''
 import re
+from pathlib import Path
 from functools import partial
-from ..filetree import filter_txt_by_regex
+
+from typing import Optional, Union
+from ..filetree import filter_txt_by_condition
 
 
 # fastest by benchmark on 12,188 atom, 2,500 frame solvated PNIPAAm
@@ -18,4 +21,8 @@ def has_water(line : str) -> bool: # 8.247 sec in benchmark
 #     regex = re.compile('(wat)|(HOH)')
 #     return bool(re.search(regex, line))
 
-strip_water = partial(filter_txt_by_regex, condition=has_water, postfix='dewatered', inclusive=False)
+# strip_water = partial(filter_txt_by_regex, condition=has_water, postfix='dewatered', inclusive=False)
+def strip_water(pdb_in : Path, pdb_out : Optional[Path]=None) -> Path: # TODO : generalize to arbitrary solvent using Solvent properties?
+    '''Create a copy of a trajectory PDB with all water residues removed
+    Returns path to copied PDB (in same dir as original if pdb_out not explicitly specified)'''
+    return filter_txt_by_condition(pdb_in, out_txt_path=pdb_out, condition=has_water, inclusive=False, postfix='dewatered', return_filtered_path=True)
