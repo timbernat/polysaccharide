1f8690cbf2a088b8db7be377e3596c842ba9670d
Timotej Bernat <tibe3324@colorado.edu>
2023-05-22 18:15:46 -0600

    Added JSONifiable monomer representation class

--- extratypes.py
+++ extratypes.py
@@ -1,18 +1,22 @@
+import numpy as np
+
+# typing and subclassing
+from .filetree import JSONifiable
 from typing import Any, Iterable, Union
-from numpy.typing import ArrayLike
-from numpy import number, ndarray
+from dataclasses import dataclass, field
 
 # Common matplotlib types
 from matplotlib.pyplot import Figure, Axes # used to avoid matplotlib import in downstream modules if only needed for typing
 from matplotlib.colors import Colormap, Normalize
 
+# Cheminformatics
 from rdkit import Chem
 
 
 # Type aliases
 ## General
-Numeric = Union[int, float, complex, number]
-ArrayLike = Union[list, tuple, ndarray, ArrayLike] # exclude dicts and sets, as they introduce complicated behaviors
+Numeric = Union[int, float, complex, np.number]
+ArrayLike = Union[list, tuple, np.ndarray, np.typing.ArrayLike] # exclude dicts and sets, as they introduce complicated behaviors
 JSONSerializable = Union[str, bool, int, float, tuple, list, dict] 
 
 ## OpenFF / graph match
@@ -23,18 +27,21 @@ RDMol  = Chem.rdchem.Mol
 RDAtom = Chem.rdchem.Atom
 RDBond = Chem.rdchem.Bond
 
-## Charge mapping and averaging - TODO : remove these, as they have been migrated over to .charging
-AtomIDMap = dict[str, dict[int, tuple[int, str]]]
+## Monomer representation (for structure maping and charging)
+ResidueSmarts = dict[str, str] # monomer SMARTS strings keyed by residue name
+AtomIDMap = dict[str, dict[int, tuple[int, str]]] 
 ChargeMap = dict[int, float] 
 ResidueChargeMap = dict[str, ChargeMap]
 
+
+# Typing functions
 def asiterable(arg_val : Union[Any, Iterable[Any]]) -> Iterable[Any]:
 	'''Permits functions expecting iterable arguments to accept singular values'''
 	if not isinstance(arg_val, Iterable):
 		arg_val = (arg_val,) # turn into single-item tuple (better for memory)
 	return arg_val
 
-# Typechecking functions
+## typechecking 
 def isnumeric(var: Any) -> bool:
 	'''Check if a variable is numerical'''
 	return isinstance(var, Numeric.__args__)
@@ -42,3 +49,39 @@ def isnumeric(var: Any) -> bool:
 def isarraylike(var: Any) -> bool:
 	'''Check if a variable is numerical'''
 	return isinstance(var, ArrayLike.__args__)
+
+
+# Custom classes / types
+@dataclass
+class MonomerInfo(JSONifiable):
+    '''For representing monomer information according to the monomer specification <eventual citation>'''
+    monomers  : ResidueSmarts
+    charges   : ResidueChargeMap = field(default=None)
+
+    @property
+    def SMARTS(self) -> ResidueSmarts:
+        return self.monomers # alias of legacy name for convenience
+    
+    @property
+    def has_charges(self) -> bool:
+        return self.charges is not None
+
+    def serialize_json_dict(unser_jdict : dict[Any, Union[ResidueSmarts, ResidueChargeMap]]) -> dict[str, JSONSerializable]:
+        '''For converting selfs __dict__ data into a form that can be serialized to JSON'''
+        return unser_jdict # no special serialization needed (writing to JSON already handles numeric-to-str conversion)
+    
+    def unserialize_json_dict(ser_jdict : dict[str, JSONSerializable]) -> dict[Any, Union[ResidueSmarts, ResidueChargeMap]]:
+        '''For unserializing charged residue maps in charged monomer JSON files'''
+        unser_jdict = {}
+        for key, value in ser_jdict.items():
+            try: # TOSELF : need try-except instead of explicit check for "charges" key since object hook is applied to ALL subdicts (not just main)
+                unser_jdict[key] = { # convert string-keyed indices and charges back to numeric types
+                    int(substruct_id) : float(charge)
+                        for substruct_id, charge in value.items()
+                }
+            except (ValueError, AttributeError):
+                unser_jdict[key] = value
+        
+        return unser_jdict
+    
+    ## TODO : implement content-aware merge method
