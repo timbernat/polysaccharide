a38950d8455d9dc5220ed89b82e7fa67cace2b22
Timotej Bernat <tibe3324@colorado.edu>
2023-05-23 12:13:08 -0600

    Added autoimports and docstrings to molutils submodules

--- molutils/polymer/__init__.py
+++ molutils/polymer/__init__.py
@@ -1,1 +1,9 @@
-from . import building, abmono
+'''Utilities for representing monomers and monomer graphs, and building polymers chains'''
+
+import pkgutil, importlib
+import logging
+
+LOGGER = logging.getLogger(__name__)
+for _loader, _module_name, _ispkg in pkgutil.iter_modules(__path__):
+    module = importlib.import_module(f'{__package__}.{_module_name}')
+    globals()[_module_name] = module # register module to namespace
--- molutils/rdmol/__init__.py
+++ molutils/rdmol/__init__.py
@@ -1,1 +1,9 @@
-from . import rdcompare, rdconvert, rdkdraw, rdprops, rdtypes
+'''Molecule manipulation utilities specific to the RDKit'''
+
+import pkgutil, importlib
+import logging
+
+LOGGER = logging.getLogger(__name__)
+for _loader, _module_name, _ispkg in pkgutil.iter_modules(__path__):
+    module = importlib.import_module(f'{__package__}.{_module_name}')
+    globals()[_module_name] = module # register module to namespace
